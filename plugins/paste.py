61fc56bdc54514257143425411db95f436d03adf
MicroDroid <yousef.su.2000@gmail.com>
2016-06-09 22:34:33 +0300

    Create paste.py

---
+++ plugins/paste.py
@@ -0,0 +1,20 @@
+import hexchat
+import requests
+import json
+
+__module_name__ = 'Paste'
+__module_author__ = 'OverCoder'
+__module_version__ = '0.0.1'
+__module_description__ = 'Pastes to pastebin and sends link'
+
+def paste(words, word_eols, userdata):
+    req = requests.post("http://p.codebottle.ml/api/v1/paste.php", data = {"code" : word_eols[1]})
+    data = json.loads(req.text)
+    if data["status"] != "200":
+        hexchat.prnt("Paste error: " + data["error"])
+    else:
+        hexchat.command("say http://" + data["rich_link"])
+
+    return hexchat.EAT_ALL
+
+hexchat.hook_command("PASTE", paste, help="/paste <content> : Pastes to pastebin and sends link")
