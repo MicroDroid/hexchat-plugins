972ecbb38c7770e0d8a90152f48692305efc3e4c
MicroDroid <yousef.su.2000@gmail.com>
2016-06-10 05:44:45 +0300

    Support multiline pastes

--- plugins/paste.py
+++ plugins/paste.py
@@ -4,17 +4,21 @@ import json
 
 __module_name__ = 'Paste'
 __module_author__ = 'OverCoder'
-__module_version__ = '0.0.1'
+__module_version__ = '0.8'
 __module_description__ = 'Pastes to pastebin and sends link'
+command = "/paste"
 
-def paste(words, word_eols, userdata):
-    req = requests.post("https://p.codebottle.ml/api/v1/paste.php", data = {"code" : word_eols[1]})
-    data = json.loads(req.text)
-    if data["status"] != "200":
-        hexchat.prnt("Paste error: " + data["error"])
-    else:
-        hexchat.command("say https://" + data["rich_link"])
+def paste(key, data, userdata):
+    if key[0] == "65293":
+        if hexchat.get_info("inputbox").startswith(command):
+            hexchat.prnt("Pasting..")
+            req = requests.post("https://p.codebottle.ml/api/v1/paste.php", data = {"code" : hexchat.get_info("inputbox")[len(command)+1:]})
+            data = json.loads(req.text)
+            if data["status"] != "200":
+                hexchat.prnt("Paste error: " + data["error"])
+            else:
+                hexchat.command("say https://" + data["rich_link"])
+            return hexchat.EAT_ALL
+    return hexchat.EAT_NONE
 
-    return hexchat.EAT_ALL
-
-hexchat.hook_command("PASTE", paste, help="/paste <content> : Pastes to pastebin and sends link")
+hexchat.hook_print("Key Press", paste)
